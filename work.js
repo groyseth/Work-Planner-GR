87aa192eea9d5af6e30f2617112af9570e84e597
Grant Royseth <groyseth@gmail.com>
2021-12-22 14:44:21 -0600

    first commit

---
+++ work.js
@@ -0,0 +1,14 @@
+var saveBtn = document.getElementById("#saveBtn");
+var hours = [
+    
+]
+
+// make an array of hours 
+// make an empty array for the text box
+// make a for loop for the hours
+// so i need to utalize the moment function 
+// need to make it responcive to the current time
+// colors are changed based on current time, futur, past
+// make a save button for the text 
+// when the page refreshes the text stays
+// a save button with pic
