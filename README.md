dc1adbb088641a5fe77cbf7854f697ba18ea8023
Grant Royseth <groyseth@gmail.com>
2021-12-23 15:15:09 -0600

    working time color and save btn

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Work-Planner-GR
+# Work-Planner-GR #
+
+I started with utializing bootstrap to get my containers with the time, textarea and save button postion
+I moved on the my 'work.js' folder to work on the javascript.
+I made a clock showing the current time on the top of the page.
+The text blocks are color coded to the current time.
--- work.js
+++ work.js
@@ -1,11 +1,75 @@
-var saveBtn = document.getElementById("#saveBtn");
-var hours = [
-    
-]
+var saveBtn = $("#saveBtn");
+var textArea = $("#saveHere");
 
-// make an array of hours 
+
+
+// sets time interval
+var myInterval = setInterval(nowTime,100);
+function nowTime (){
+var now = moment().format("MMM Do, YYYY,  h:mm:ss a")
+$("#currentDay").text(now);
+}
+
+
+// sets color to time
+function timeColor() {
+    var currentTime = moment().hours();
+    $(".time-block").each(function () {
+        var timeBlock = parseInt($(this).attr("id").split(" ")[0]);
+        if (timeBlock < currentTime) {
+            $(this).addClass("past");
+        } else if (timeBlock === currentTime) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+        
+    })
+}
+
+
+
+
+
+// saveBtn.addEventListener("click", function(){
+    
+//     textArea = localStorage.setItem("save", textArea);
+//     console.log("save")
+
+// })
+
+$(document).ready(function(){
+ $(".saveBtn").click(function(){
+    var textInput = $(this).siblings(".description").val();
+    var timeNum = $(this).prev().attr("id");
+    localStorage.setItem(timeNum,textInput );
+    console.log(this);
+}); 
+})
+for (i = 9; i < 18; i++) {
+    var timeNum = localStorage.getItem(i);
+    var textInput = $("#" + i);
+    textInput.val(timeNum);
+    console.log(textInput)
+}
+  
+
+
+timeColor();
+
+
+
+
+
+
+
 // make an empty array for the text box
 // make a for loop for the hours
+// cycle hours and 
+
 // so i need to utalize the moment function 
 // need to make it responcive to the current time
 // colors are changed based on current time, futur, past
